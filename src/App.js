142066b14ba22b2eb253e7672dc78a9bf32f0b80
crispimluiz <crispimluiz@live.com>
2021-03-23 14:34:16 -0300

    Start and Headers

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 
 import Site from './site'
 import Footers from './components/Footers'
+import Headers from './components/Headers'
+import Start from './components/Start'
 
 class App extends Component {
   render() {
     return (
       <div className="App" >
+        <Headers />
+        <Start />
         <Site />
         <Footers />
       </div >
