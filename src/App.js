e628ea3bf3b713428e01bc2e9fb1cc39cc8aea72
crispimluiz <crispimluiz@live.com>
2021-03-23 14:20:22 -0300

    Rodapé - Footeer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import Site from './site'
+import Footers from './components/Footers'
 
 class App extends Component {
   render() {
     return (
       <div className="App" >
         <Site />
+        <Footers />
       </div >
     );
   }
---
+++ src/components/Footers.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footers = props => {
+  return (
+    <footer className='container-fluid text-center'>
+      <a href='#myPage' title='To Top'>
+        <span className='glyphicon glyphicon-chevron-up'></span>
+      </a>
+      <p>Site Crispim </p>
+    </footer>
+  )
+}
+
+export default Footers;
